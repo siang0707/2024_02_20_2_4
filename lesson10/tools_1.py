8884e6b4b3585e91f694950a487f24cd85527427
謝景翔 <160603311+siang0707@users.noreply.github.com>
2024-03-21 11:32:05 +0000

    modify

--- lesson10/tools_1.py
+++ lesson10/tools_1.py
@@ -1,4 +1,5 @@
-from tools_0 import getItems,Items #自訂的Model執行方式，現在另一個檔案完成其內容資料，最後採用自訂Model來簡易執行輸出
+from tools_0 import getItems,Items 
+#自訂的Model執行方式，現在另一個檔案完成其內容資料，最後採用自訂Model來簡易執行輸出
 
 def main():
     items:Items = getItems()
