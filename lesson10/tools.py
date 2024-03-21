5e0887c2c2c9bf0e139c86711431d9d804e53d2c
謝景翔 <160603311+siang0707@users.noreply.github.com>
2024-03-21 13:44:40 +0000

    modify

--- lesson10/tools.py
+++ lesson10/tools.py
@@ -1,8 +1,20 @@
 import random
 
 def get_names(nums:int = 5) -> list[str]:
-    with open('names.txt',encoding="utf8") as file:
+    with open('names.txt',encoding='utf8') as file:
         content:str = file.read()
         names:list[str] = content.split(sep='\n')
-        random_name:list[str] = random.choices(names,k=nums)
-    return random_name
+        random_names:list[str] = random.choices(names,k=nums)
+    return random_names
+
+
+def get_scores(names:list[str]) -> list[dict]:
+    stus:list[dict] = []
+    for name in names:
+        stu:dict[str:any] = {}
+        stu["name"] = name
+        stu["chinese"] = random.randint(50,100)
+        stu["english"] = random.randint(50,100)
+        stu["math"] = random.randint(50,100)
+        stus.append(stu)
+    return stus
