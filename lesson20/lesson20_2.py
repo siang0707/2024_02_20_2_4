55b1bd1afefb4ca757e195f2e66fa1b5080d1360
siang0707 <b8730707@gmail.com>
2024-05-02 13:17:40 +0000

    modify

--- lesson20/lesson20_2.py
+++ lesson20/lesson20_2.py
@@ -34,9 +34,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    response = model.generate_content(event.message.text) #event.message.text為對方傳送的內容訊息包裝成response
-    message = TextSendMessage(text=response.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    try:
+        response = model.generate_content(event.message.text)
+        #event.message.text為對方傳送的內容訊息包裝成response
+        message = TextSendMessage(text=response.text)
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        message = TextSendMessage(text="發生不明錯誤!請稍待一下再重試")
+        line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
